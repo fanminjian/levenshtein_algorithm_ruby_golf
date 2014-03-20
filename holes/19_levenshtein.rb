0ab14e47a584979ec702e6586222c42519b312f7
Jimmy Fan <fanminjian@gmail.com>
2014-03-20 21:03:55 +1300

    MOre

--- holes/19_levenshtein.rb
+++ holes/19_levenshtein.rb
@@ -1,16 +1,13 @@
 def d(o, p)
   f, s = [o, p].map{ |s| s.encode(Encoding::UTF_8).unpack("U*") }
   q = [(0..f.size).to_a]
-  (1..s.size).each {|j| q << [j] + [0] * (f.size) }
-  (1..s.size).each { |i|
-    (1..f.size).each { |j|
+  (1..s.size).map {|j| q << [j] + [0] * (f.size) }
+  (1..s.size).map { |i|
+    (1..f.size).map { |j|
       q[i][j] = f[j-1] == s[i-1] ? q[i-1][j-1] : [q[i-1][j],q[i][j-1],q[i-1][j-1],].min + 1
     }
   }
   q.last.last
 end
 
-while l = gets do
-  a, b = l.split
-  puts d a.to_s, b.to_s
-end
+while l=gets;a,b=l.split;puts d a,b;end
