9eb68345ec1d5ea8469ba5adb72b3f9a48d03d58
Jimmy Fan <fanminjian@gmail.com>
2014-03-20 20:26:10 +1300

    Second

--- holes/19_levenshtein.rb
+++ holes/19_levenshtein.rb
@@ -1,107 +1,67 @@
-module Text
-module Levenshtein
-  def distance(str1, str2, max_distance = nil)
-    if max_distance
-      distance_with_maximum(str1, str2, max_distance)
-    else
-      distance_without_maximum(str1, str2)
-    end
-  end
-private
-  def distance_with_maximum(str1, str2, max_distance) # :nodoc:
-    s, t = [str1, str2].sort_by(&:length).
-                        map{ |str| str.encode(Encoding::UTF_8).unpack("U*") }
-    n = s.length
-    m = t.length
-    big_int = n * m
-    return m if n.zero?
-    return n if m.zero?
-    return 0 if s == t
-
-    if (n - m).abs >= max_distance
-      return max_distance
-    end
-
-    d = (m + 1).times.map { |i|
-      if i < m || i < max_distance + 1
-        i
-      else
-        big_int
-      end
-    }
-    x = nil
-    e = nil
-
-    n.times do |i|
-      if e.nil?
-        e = i + 1
-      else
-        e = big_int
-      end
-
-      diag_index = t.length - s.length + i
-
-      min = [0, i - max_distance - 1].max
-      max = [m - 1, i + max_distance].min
-
-      (min .. max).each do |j|
-        if j == diag_index && d[j] >= max_distance
-          return max_distance
-        end
-
-        cost = s[i] == t[j] ? 0 : 1
-        x = [
-          d[j+1] + 1, # insertion
-          e + 1,      # deletion
-          d[j] + cost # substitution
-        ].min
-
-        d[j] = e
-        e = x
-      end
-      d[m] = x
-    end
-
-    if x > max_distance
-      return max_distance
-    else
-      return x
-    end
-  end
-
-  def distance_without_maximum(str1, str2) # :nodoc:
-    first, second = [str1, str2].map{ |str| str.encode(Encoding::UTF_8).unpack("U*") }
-    n = first.length
-    m = second.length
-    return m if n.zero?
-    return n if m.zero?
-
-    matrix = [(0..first.length).to_a]
-    (1..second.length).each do |j|
-      matrix << [j] + [0] * (first.length)
-    end
- 
-    (1..second.length).each do |i|
-      (1..first.length).each do |j|
-        if first[j-1] == second[i-1]
-          matrix[i][j] = matrix[i-1][j-1]
-        else
-          matrix[i][j] = [
-            matrix[i-1][j],
-            matrix[i][j-1],
-            matrix[i-1][j-1],
-          ].min + 1
-        end
-      end
-    end
-    return matrix.last.last
-  end
-
-  extend self
+def d(x, y, m = nil)
+  m ? dwm(x, y, m) : dom(x, y)
 end
+
+def dwm(str1, str2, max)
+  s, t = [str1, str2].sort_by(&:length).map{ |str| str.encode(Encoding::UTF_8).unpack("U*") }
+  n = s.length
+  m = t.length
+  big_int = n * m
+  return m if n.zero?
+  return n if m.zero?
+  return 0 if s == t
+
+  return max if (n - m).abs >= max
+
+  d = (m + 1).times.map { |i| i < m || i < max + 1 ? i : big_int }
+  x,e = nil
+
+  n.times do |i|
+    e = e.nil? ? i + 1 : big_int
+
+    diag_index = t.length - s.length + i
+
+    min = (0..i-max-1).max
+    max = [m - 1, i + max].min
+
+    (min .. max).each do |j|
+      return max if j == diag_index && d[j] >= max
+
+      cost = s[i] == t[j] ? 0 : 1
+      x = [
+        d[j+1] + 1, # insertion
+        e + 1,      # deletion
+        d[j] + cost # substitution
+      ].min
+
+      d[j] = e
+      e = x
+    end
+    d[m] = x
+  end
+
+  return x > max ? max : x
+end
+
+def dom(str1, str2)
+  first, second = [str1, str2].map{ |str| str.encode(Encoding::UTF_8).unpack("U*") }
+  n = first.length
+  m = second.length
+  return m if n.zero?
+  return n if m.zero?
+
+  matrix = [(0..first.length).to_a]
+  (1..second.length).each {|j| matrix << [j] + [0] * (first.length) }
+
+  (1..second.length).each { |i|
+    (1..first.length).each { |j|
+      first[j-1] == second[i-1] ? matrix[i][j] = matrix[i-1][j-1] : matrix[i][j] = [matrix[i-1][j],matrix[i][j-1],matrix[i-1][j-1],].min + 1
+    }
+  }
+  return matrix.last.last
 end
 
 while line = gets do
-  word1, word2 = line.split
-  puts Text::Levenshtein.distance word1.to_s, word2.to_s
+  a, b = line.split
+  puts d a.to_s, b.to_s
 end
