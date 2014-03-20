af321a1174b71893e4f880c0851e575b5a5c2fae
Jimmy Fan <fanminjian@gmail.com>
2014-03-20 21:35:19 +1300

    Encode

--- holes/19_levenshtein.rb
+++ holes/19_levenshtein.rb
@@ -1,2 +1,1 @@
-def d(o,p)f,s=[o,p].map{|s|s.encode.unpack("U*")};q=[(0..f.size).to_a];(1..s.size).map{|j|q<<[j]+[0]*(f.size)};(1..s.size).map{|i|(1..f.size).map{|j|q[i][j]=f[j-1]==s[i-1] ? q[i-1][j-1]:[q[i-1][j],q[i][j-1],q[i-1][j-1],].min+1}};q.last.last;end
-$<.map{|l|a,b=l.split;p d a,b}
+def d(o,p)f,s=[o,p].map{|s|s.unpack("U*")};q=[(0..f.size).to_a];(1..s.size).map{|j|q<<[j]+[0]*(f.size)};(1..s.size).map{|i|(1..f.size).map{|j|q[i][j]=f[j-1]==s[i-1] ? q[i-1][j-1]:[q[i-1][j],q[i][j-1],q[i-1][j-1],].min+1}};q.last.last;end;$<.map{|l|a,b=l.split;p d a,b}
