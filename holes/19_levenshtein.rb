113f50c5d7ec4148b8e57837a66c21f654d72b37
Jimmy Fan <fanminjian@gmail.com>
2014-03-20 20:38:47 +1300

    Third.

--- holes/19_levenshtein.rb
+++ holes/19_levenshtein.rb
@@ -4,8 +4,8 @@ end
 
 def dwm(str1, str2, max)
   s, t = [str1, str2].sort_by(&:length).map{ |str| str.encode(Encoding::UTF_8).unpack("U*") }
-  n = s.length
-  m = t.length
+  n = s.size
+  m = t.size
   big_int = n * m
   return m if n.zero?
   return n if m.zero?
@@ -24,44 +24,39 @@ def dwm(str1, str2, max)
     min = (0..i-max-1).max
     max = [m - 1, i + max].min
 
-    (min .. max).each do |j|
+    (min..max).each do |j|
       return max if j == diag_index && d[j] >= max
 
       cost = s[i] == t[j] ? 0 : 1
-      x = [
-        d[j+1] + 1, # insertion
-        e + 1,      # deletion
-        d[j] + cost # substitution
-      ].min
-
+      x = [d[j+1] + 1,e + 1,d[j] + cost].min
       d[j] = e
       e = x
     end
     d[m] = x
   end
 
-  return x > max ? max : x
+  x > max ? max : x
 end
 
 def dom(str1, str2)
   first, second = [str1, str2].map{ |str| str.encode(Encoding::UTF_8).unpack("U*") }
-  n = first.length
-  m = second.length
+  n = first.size
+  m = second.size
   return m if n.zero?
   return n if m.zero?
 
-  matrix = [(0..first.length).to_a]
-  (1..second.length).each {|j| matrix << [j] + [0] * (first.length) }
+  ma = [(0..first.size).to_a]
+  (1..second.size).each {|j| ma << [j] + [0] * (first.size) }
 
-  (1..second.length).each { |i|
-    (1..first.length).each { |j|
-      first[j-1] == second[i-1] ? matrix[i][j] = matrix[i-1][j-1] : matrix[i][j] = [matrix[i-1][j],matrix[i][j-1],matrix[i-1][j-1],].min + 1
+  (1..second.size).each { |i|
+    (1..first.size).each { |j|
+      ma[i][j] = first[j-1] == second[i-1] ? ma[i-1][j-1] : [ma[i-1][j],ma[i][j-1],ma[i-1][j-1],].min + 1
     }
   }
-  return matrix.last.last
+  return ma.last.last
 end
 
-while line = gets do
-  a, b = line.split
+while l = gets do
+  a, b = l.split
   puts d a.to_s, b.to_s
 end
